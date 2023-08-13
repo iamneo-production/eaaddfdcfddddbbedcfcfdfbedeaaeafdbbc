1dd1081a1948d1b6a6edd77d52a531676d422d9e
Iamneo <techadmin@iamneo.ai>
2023-08-13 11:11:09 +0000

    Backup Commit

--- reactapp/src/setupTests.js
+++ reactapp/src/setupTests.js
@@ -1,9 +1,3 @@
-import '@testing-library/jest-dom';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
